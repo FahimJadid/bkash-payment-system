a1dc4b1f7ce8c7d645d8ae34e519379ba62c1db6
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2024-01-15 23:51:45 +0600

    added frontend

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const Home = () => {
+  return (
+    <>
+      <h1>Bkash Payment System</h1>
+      <div className="card">
+        <button>Pay with Bkash</button>
+      </div>
+    </>
+  );
+};
+
+export default Home;
